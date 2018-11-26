5fcb73bda84908212e7b05cc5dbb0742cf0c7651
Jia Cheng Ong <20966172+JiaChengOng96@users.noreply.github.com>
2018-11-25 22:49:25 -0500

    update the second requirement

---
+++ script.js
@@ -0,0 +1,7 @@
+function myButton()
+{
+    if (document.getElementById("para").style.color != red)
+        document.getElementById("para").style.color = red;
+    else
+        document.getElementById("para").style.color = yellow;
+}
