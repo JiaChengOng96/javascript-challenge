f469736191d171550d5c7ff0cdfa76f67a8e41c8
Jia Cheng Ong <20966172+JiaChengOng96@users.noreply.github.com>
2018-11-25 22:58:15 -0500

    complete the requirement

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function myButton()
 {
-    if (document.getElementById("para").style.color != red)
-        document.getElementById("para").style.color = red;
+    if (document.getElementById("para").style.color != "red") {
+        document.getElementById("para").style.color = "red";
+    }
     else
-        document.getElementById("para").style.color = yellow;
+        {document.getElementById("para").style.color = "green";
+    }
 }
